4021d1a4e7a78a9ac499ffb00549b14c4b2a98e0
riyanris <riyanrisky129@gmail.com>
2021-09-12 18:12:04 +0700

    hs

--- helpers/downloader.js
+++ helpers/downloader.js
@@ -990,7 +990,7 @@ module.exports.ttdl = (url) => {
 
 module.exports.ttdl2 = (url) => {
   return new Promise((resolve, reject) => {
-    let baseurl = 'https://ssstik.io'
+    let baseurl = 'https://ssstik.io's
     axios.get(baseurl, {
       withCredentials: true,
       headers: {
